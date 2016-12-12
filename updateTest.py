6a7a4306492c0c53ac541655ece25ccd64d68999
NanamiTakayama <ns.potable@gmail.com>
2016-12-13 03:11:22 +0900

    2016-12-13 03:11:22.083158

--- updateTest.py
+++ updateTest.py
@@ -3,6 +3,6 @@ import datetime
 date = str(datetime.datetime.today())
 
 while True:
-    print("Generated at" + "2016-12-13 03:07:33.046972")
+    print("Generated at" + "2016-12-13 03:11:22.083158")
     print(date)
     time.sleep(20)
