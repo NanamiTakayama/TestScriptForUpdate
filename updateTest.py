854a8cff9f18bda85022bb42a90d0423d1c38f61
NanamiTakayama <ns.potable@gmail.com>
2016-12-13 15:42:25 +0900

    2016-12-13 15:42:25.351754

--- updateTest.py
+++ updateTest.py
@@ -1,8 +1,15 @@
 import time
 import datetime
+import commands
+from slacker import Slacker
+
 date = str(datetime.datetime.today())
 
 while True:
-    print("Generated at" + "2016-12-13 13:21:37.064267")
+    systate = str(commands.getoutput("ps aux | grep isaax"))
+    netstate = str(commands.getoutput("ping 8.8.8.8"))
+    print(netstate)
+    print(systate)
+    print("Generated at" + "2016-12-13 15:42:25.351754")
     print(date)
-    time.sleep(20)
+    time.sleep(30)
