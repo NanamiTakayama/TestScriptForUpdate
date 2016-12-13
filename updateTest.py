2cac3626568434ad2f9cad1343937c4d8e314dc5
NanamiTakayama <ns.potable@gmail.com>
2016-12-13 15:50:28 +0900

    2016-12-13 15:50:28.606484

--- updateTest.py
+++ updateTest.py
@@ -1,3 +1,6 @@
+#coding:utf-8
+
+print("App started")
 import time
 import datetime
 import commands
@@ -10,6 +13,6 @@ while True:
     netstate = str(commands.getoutput("ping 8.8.8.8"))
     print(netstate)
     print(systate)
-    print("Generated at" + "2016-12-13 15:45:07.768738")
+    print("Generated at" + "2016-12-13 15:50:28.606484")
     print(date)
     time.sleep(30)
