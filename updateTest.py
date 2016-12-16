a39ae6337963b8489f80727d6c200215e460a2eb
NanamiTakayama <ns.potable@gmail.com>
2016-12-16 13:17:12 +0900

    2016-12-16 13:17:12.017488

--- updateTest.py
+++ updateTest.py
@@ -45,7 +45,7 @@ slack = Slack("xoxp-8287559173-9517193584-113147450805-baefd83f3c433756418e7f31b
 
 while True:
     slack.post_message_to_channel("monitoring", "test")
-    print("Generated at" + "2016-12-16 12:26:40.532407")
+    print("Generated at" + "2016-12-16 13:17:12.017488")
     systate = str(commands.getoutput("ps aux | grep isaax"))
     print(systate)
     print(date)
